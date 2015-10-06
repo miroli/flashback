43fc78afbdd6f5b9c2cb3825c7158627abfa684c
vienno <robin.linderborg@gmail.com>
2015-10-07 00:24:57 +0200

    added install_requires

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     description='The handiest Flashback scraper in the game',
     author='Robin Linderborg',
     author_email='robin.linderborg@gmail.com',
+    install_requires=[
+        'beautifulsoup4==4.4.1',
+        'requests==2.8.0'
+    ],
     url='https://github.com/vienno/flashback',
     download_url='https://github.com/vienno/flashback/tarball/0.1',
     keywords=['flashback', 'scraping'],
