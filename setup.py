b92fa8c8905e987cccdff0f4f419eb92b38b0c8b
vienno <robin.linderborg@gmail.com>
2015-11-09 15:00:18 +0100

    updated to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='flashback',
     packages=['flashback'],
-    version='0.1.1',
+    version='0.2',
     description='The handiest Flashback scraper in the game',
     author='Robin Linderborg',
     author_email='robin.linderborg@gmail.com',
@@ -11,7 +11,7 @@ setup(
         'requests==2.8.0'
     ],
     url='https://github.com/vienno/flashback',
-    download_url='https://github.com/vienno/flashback/tarball/0.1',
+    download_url='https://github.com/vienno/flashback/tarball/0.2',
     keywords=['flashback', 'scraping'],
     classifiers=[],
 )
